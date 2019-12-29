39585ebc21f5645abc3083276e6ec13eef2b1c98
Le Toan <toanlk@sapo.vn>
2019-12-29 21:15:20 +0700

    [Add Feature] Init

---
+++ DemoIOSDevops/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DemoIOSDevops
+//
+//  Created by Le Toan on 12/29/19.
+//  Copyright © 2019 LeToan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ DemoIOSDevops/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DemoIOSDevops
+//
+//  Created by Le Toan on 12/29/19.
+//  Copyright © 2019 LeToan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
